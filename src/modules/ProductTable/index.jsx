767865b6737b2b07d6299499b34aa3c917cb78e7
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-06-05 14:45:23 -0400

    Completed Functionality!

--- src/modules/ProductTable/index.jsx
+++ src/modules/ProductTable/index.jsx
@@ -1,11 +1,37 @@
-import {PRODUCTS} from '../../data/products'
+import style from './ProductTable.module.css'
+import ProductCategoryRow from '../ProductCategoryRow';
+import ProductRow from '../ProductRow';
 
-function ProductTable(){
-    console.log(PRODUCTS);
+function ProductTable({products, checked, search}){
+    const categories = [];
+    const builtList = [];
+    products.forEach(product => {
+        if(checked && !product.stocked) return;
+        if (
+            product.name.toLowerCase().includes(search.toLowerCase())
+            &&
+            (!categories.includes(product.category))) {
+                categories.push(product.category)
+            }
+        });
+    categories.forEach(category => {
+        builtList.push(<ProductCategoryRow category={category}/>)
+        products.forEach(prod =>{
+            if(checked && !prod.stocked) return;
+            if(
+                (prod.category === category)
+                &&
+                prod.name.toLowerCase().includes(search.toLowerCase())) {
+                    builtList.push(<ProductRow name={prod.name} price={prod.price} stocked={prod.stocked}/>) 
+                }
+        })
+    });
+
     return(
-        <>
-        <p>Product Table</p>
-        </>
+        <ul>
+            <li className={style.listItem}><p>Name</p><p>Price</p></li>
+            {builtList}
+        </ul>
     );
 }
 export default ProductTable;
